cf6b50a25074248470bf39a94dc7add62e2c7746
Sanfras <70115309+Sanfras@users.noreply.github.com>
2020-08-26 21:52:06 -0300

    Add files via upload

---
+++ Assets/Scripts/BackgroundScaler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class BackgroundScaler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 starScale;
+    
+    void Start()
+    {
+        starScale = transform.localScale;
+
+       
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        
+        transform.localScale = new Vector3(width, height, starScale.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Backtexan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Backtexan : MonoBehaviour
+{
+    private Material mat;
+    private Vector2 offset;
+
+    [SerializeField]
+    public float speed = 0.2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        offset = mat.GetTextureOffset("_MainTex");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset.x = offset.x + (speed * Time.deltaTime);
+        mat.SetTextureOffset("_MainTex", offset);
+        mat.SetTextureOffset("_MainTex", offset);
+
+
+    }
+}
