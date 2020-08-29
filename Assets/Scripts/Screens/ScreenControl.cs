ad6348fbc8509421beb4cfecc3e5db888769d460
Maria Fernanda <feassis2001@hotmail.com>
2020-08-29 18:13:57 -0300

    qualquercoisa

---
+++ Assets/Scripts/Screens/ScreenControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenControl : MonoBehaviour
+{
+
+    public void CallScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}   
